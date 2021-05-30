27f2e107400ec7b0eabe1b4b7fdefaf0348e57ea
Rafael-Fonseca <rafael.abreu.unb@gmail.com>
2021-05-29 21:14:38 -0300

    Criado arquivos para publicação no Heroku

--- curso_django/settings.py
+++ curso_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s-cw=$^t9t8d1m*79xlmthv*#)ijd-p4s8g+9svg7crq7wc!c^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
