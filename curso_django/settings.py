d37a3615ee2a8197500ff8177a043d152f07c316
Rafael-Fonseca <rafael.abreu.unb@gmail.com>
2021-05-31 17:28:24 -0300

    Extraído configurações para secret key

--- curso_django/settings.py
+++ curso_django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s-cw=$^t9t8d1m*79xlmthv*#)ijd-p4s8g+9svg7crq7wc!c^'
+SECRET_KEY = config('')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
