4a473e6230b78bf1609b621c9968bec8f6a1d968
Rafael-Fonseca <rafael.abreu.unb@gmail.com>
2021-06-01 19:47:33 -0300

    Criação e migração do Modelo User

--- curso_django/settings.py
+++ curso_django/settings.py
@@ -31,10 +31,10 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
+# Application definition
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-
-# Application definition
+AUTH_USER_MODEL = 'base.User'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
